fda48d44a9366829906975f8bdd5b430a0dc27a9
joeson <646608023@qq.com>
2020-04-15 16:05:46 +0800

    init: init project

---
+++ android/src/main/kotlin/com/rkjoeson/flutter_contact_picker/MethodConstants.kt
@@ -0,0 +1,10 @@
+package com.rkjoeson.flutter_contact_picker
+
+object MethodConstants {
+
+    // 跳转原生选择联系人页面
+    const val METHOD_CALL_GO_TO_CONTACT = "selectContactNative"
+
+    // 获取联系人列表
+    const val METHOD_CALL_LIST = "selectContactList"
+}
---
+++ android/src/main/kotlin/com/rkjoeson/flutter_contact_picker/ResponseCallback.kt
@@ -0,0 +1,6 @@
+package com.rkjoeson.flutter_contact_picker
+
+@FunctionalInterface
+interface ResponseCallback {
+    fun send(data: Any?)
+}
